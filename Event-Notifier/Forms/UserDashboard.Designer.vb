bc2a507390569cbcf3a0779e29c0dde57e426113
Kitsune254 <owokoselwyn@gmail.com>
2026-01-08 13:05:19 +0300

    changed a lot of files

--- Event-Notifier/Forms/UserDashboard.Designer.vb
+++ Event-Notifier/Forms/UserDashboard.Designer.vb
@@ -22,9 +22,15 @@ Partial Class UserDashboard
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "UserDashboard"
+        SuspendLayout()
+        ' 
+        ' UserDashboard
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Name = "UserDashboard"
+        Text = "UserDashboard"
+        ResumeLayout(False)
     End Sub
 End Class
